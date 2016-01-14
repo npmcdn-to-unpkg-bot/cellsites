cbc8bbc1a0b24672f40bc4549a6e886f81baed64
James Pole <james@pole.net.nz>
2016-01-14 21:20:02 +1300

    Backport const for Network tyes

--- src/CellSites/Database/Network.php
+++ src/CellSites/Database/Network.php
@@ -5,6 +5,28 @@ namespace CellSites\Database;
 use CellSites\Database\Base\Network as BaseNetwork;
 
 class Network extends BaseNetwork {
+    
+    const GSM = 1;
+    const UMTS = 2;
+    const LTE = 4;
+    
+    public function countCells($type) {
+        
+        if($type === self::UMTS) {
+            
+            return($this->countCellUMTSs());
+            
+        } elseif($type === self::LTE) {
+            
+            return($this->countCellLTEs());
+            
+        } else {
+            
+            throw new LogicException;
+            
+        }
+        
+    }
 
 	public function getFullName() {
 
