6f0cbc6597ae01f366a0480780452eb6de01a3df
James Pole <james@pole.net.nz>
2016-01-14 22:52:35 +1300

    Refactored GeoJSON-related classes

--- src/CellSites/Web/LocationGeoJSON.php
+++ src/CellSites/Web/LocationGeoJSON.php
@@ -6,17 +6,27 @@ use CellSites\Database\LocationQuery;
 
 class LocationGeoJSON extends GeoJSON {
 
-	public function __construct($locationID) {
+    public function __construct($locationID) {
 
-		if(is_int($locationID) !== true) {
+        if(is_int($locationID) !== TRUE) {
 
-			throw New Exception;
+            throw new \LogicException('The given Location ID must be an integer.');
 
-		}
+        }
 
-		$this->setLocations(LocationQuery::create()->filterByID($locationID)->find());
+        $query = LocationQuery::create()->filterByID($locationID);
 
-	}
+        if($query->count() === 0) {
+
+            $page = new ResourceNotFoundPage;
+            $page->generate();
+            exit;
+
+        }
+
+        parent::__construct($query->find());
+
+    }
 
 }
 
--- src/CellSites/Web/RegionGeoJSON.php
+++ src/CellSites/Web/RegionGeoJSON.php
@@ -6,17 +6,27 @@ use CellSites\Database\LocationQuery;
 
 class RegionGeoJSON extends GeoJSON {
 
-	public function __construct($regionID) {
+    public function __construct($regionID) {
 
-		if(is_int($regionID) !== true) {
+        if(is_int($regionID) !== TRUE) {
 
-			throw new Exception;
+            throw new \LogicException('The given Region ID must be an integer.');
 
-		}
+        }
 
-		$this->setLocations(LocationQuery::create()->filterByRegionID($regionID)->find());
+        $query = LocationQuery::create()->filterByRegionID($regionID);
 
-	}
+        if($query->count() === 0) {
+
+            $page = new ResourceNotFoundPage;
+            $page->generate();
+            exit;
+
+        }
+
+        parent::__construct($query->find());
+
+    }
 
 }
 
