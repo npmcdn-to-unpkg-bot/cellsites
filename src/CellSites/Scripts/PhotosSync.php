b065ebb6cfdda4a1073a1bec2e2bf7d8afcb79ec
James Pole <james@pole.net.nz>
2016-01-17 12:30:07 +1300

    Refactor PhotosSync and add deletePhotos()

--- src/CellSites/Scripts/PhotosSync.php
+++ src/CellSites/Scripts/PhotosSync.php
@@ -7,98 +7,123 @@ use CellSites\Database\PhotoQuery;
 use DateTime;
 use Imagick;
 use ImagickDraw;
+use LogicException;
 
 class PhotosSync {
 
-	private $options = null;
-	private $originalsDirectory = null;
-	private $photosDirectory = null;
-	private $thumbnailsDirectory = null;
+    const ORIGINAL = 1;
+    const PHOTO = 2;
+    const THUMBNAIL = 3;
 
-	public function __construct() {
+    private $options = NULL;
+    private $originalsDirectory = NULL;
+    private $photosDirectory = NULL;
+    private $thumbnailsDirectory = NULL;
 
-		$this->options = getopt('dghpt');
+    public function __construct() {
 
-	}
+        $this->options = getopt('dghopt');
 
-	private function checkOriginalsDirectory() {
+        if(defined('CELLSITES_PHOTOS_ORIGNALS_DIR') !== TRUE) {
 
-		echo('Checking originals directory...' . PHP_EOL);
+            echo('Notice: Configuration constant CELLSITES_PHOTOS_ORIGNALS_DIR has not been defined.' . PHP_EOL . PHP_EOL);
 
-		if(is_dir(CELLSITES_PHOTOS_ORIGNALS_DIR) !== true) {
+        }
 
-			echo('Originals directory ' . CELLSITES_PHOTOS_ORIGNALS_DIR . ' does not exist. Please check your CELLSITES_PHOTOS_ORIGNALS_DIR configuration.');
+        if(defined('CELLSITES_PHOTOS_TARGET_DIR') !== TRUE) {
 
-		}
+            echo('Notice: Configuration constant CELLSITES_PHOTOS_TARGET_DIR has not been defined.' . PHP_EOL . PHP_EOL);
 
-		$this->originalsDirectory = dir(CELLSITES_PHOTOS_ORIGNALS_DIR);
-		echo('Using originals directory ' . CELLSITES_PHOTOS_ORIGNALS_DIR . '.' . PHP_EOL . PHP_EOL);
+        }
 
-	}
+    }
 
-	private function checkPhotosDirectory() {
+    private function checkDirectory($type) {
 
-		echo('Checking photos directory...' . PHP_EOL);
+        if($type === self::ORIGINAL) {
 
-		$photosDirectory = CELLSITES_PHOTOS_TARGET_DIR . '1280x720/';
+            if(defined('CELLSITES_PHOTOS_ORIGNALS_DIR') !== TRUE) {
 
-		if(is_dir($photosDirectory) !== true) {
+                echo('Skipping check of originals directory because constant CELLSITES_PHOTOS_ORIGNALS_DIR has not been defined.' . PHP_EOL . PHP_EOL);
+                return;
 
-			if(isset($this->options['p']) !== true) {
+            }
 
-				echo('Photos directory ' . $photosDirectory . ' does not exist. Run with -p flag to create.' . PHP_EOL);
-				exit;
+            $option = 'o';
+            $plural = 'originals';
+            $pluralCapitalised = 'Originals';
+            $directory =& $this->originalsDirectory;
+            $directoryPath = CELLSITES_PHOTOS_ORIGNALS_DIR;
 
-			}
+        } elseif($type === self::PHOTO || $type === self::THUMBNAIL) {
 
-			if(mkdir($photosDirectory,0777,true) !== true) {
+            if(defined('CELLSITES_PHOTOS_TARGET_DIR') !== TRUE) {
 
-				echo('Could not create photos directory ' . $photosDirectory . '.' . PHP_EOL);
-				exit;
+                echo('Skipping check of ' . $plural . ' directory because constant CELLSITES_PHOTOS_TARGET_DIR has not been defined.' . PHP_EOL);
+                return;
 
-			}
+            }
 
-			echo('Photos directory ' . $photosDirectory . ' has been created.' . PHP_EOL);
+            if($type === self::PHOTO) {
 
-		}
+                $option = 't';
+                $plural = 'photos';
+                $pluralCapitalised = 'Photos';
+                $directory =& $this->photosDirectory;
+                $directoryPath = CELLSITES_PHOTOS_TARGET_DIR . '1280x720/';
 
-		$this->photosDirectory = dir($photosDirectory);
-		echo('Using photos directory ' . $photosDirectory . '.' . PHP_EOL . PHP_EOL);
+            } elseif($type === self::THUMBNAIL) {
 
-	}
+                $option = 'p';
+                $plural = 'thumbnails';
+                $pluralCapitalised = 'Thumbnails';
+                $directory =& $this->thumbnailsDirectory;
+                $directoryPath = CELLSITES_PHOTOS_TARGET_DIR . '243x161/';
 
-	private function checkThumbnailsDirectory() {
+            }
 
-		echo('Checking thumbnails directory...' . PHP_EOL);
+        } else {
 
-		$thumbnailsDirectory = CELLSITES_PHOTOS_TARGET_DIR . '243x161/';
+            throw new LogicException('Given type must be either a ORIGINAL, PHOTO or THUMBNAIL.');
 
-		if(is_dir($thumbnailsDirectory) !== true) {
+        }
 
-			if(isset($this->options['t']) !== true) {
+        echo('Checking ' . $plural . ' directory...' . PHP_EOL);
 
-				echo('Thumbnails directory ' . $thumbnailsDirectory . ' does not exist. Run with -t flag to create.' . PHP_EOL);
-				exit;
+        if(is_dir($directoryPath) !== true) {
 
-			}
+            if(isset($this->options[$option]) !== true) {
 
-			if(mkdir($thumbnailsDirectory,0777,true) !== true) {
+                echo($pluralCapitalised . ' directory ' . $directoryPath . ' does not exist. Run with -' . $option . ' flag to create.' . PHP_EOL);
+                exit;
 
-				echo('Could not create thumbnails directory ' . $thumbnailsDirectory . '.' . PHP_EOL);
-				exit;
+            }
 
-			}
+            if(mkdir($directoryPath,0777,true) !== true) {
 
-			echo('Thumbnails directory ' . $thumbnailsDirectory . ' has been created.' . PHP_EOL);
+                echo('Could not create ' . $plural . ' directory ' . $directoryPath . '.' . PHP_EOL);
+                exit;
 
-		}
+            }
 
-		$this->thumbnailsDirectory = dir($thumbnailsDirectory);
-		echo('Using thumbnails directory ' . $thumbnailsDirectory . '.' . PHP_EOL . PHP_EOL);
+            echo($pluralCapitalised . ' directory ' . $directoryPath . ' has been created.' . PHP_EOL);
 
-	}
+        }
 
-	private function createDatabaseEntries() {
+        $directory = dir($directoryPath);
+
+        echo('Using ' . $plural . ' directory ' . $directoryPath . '.' . PHP_EOL . PHP_EOL);
+
+    }
+
+    private function createDatabaseEntries() {
+
+        if(defined('CELLSITES_PHOTOS_ORIGNALS_DIR') !== TRUE) {
+
+            echo('Skipping creation of database entries because constant CELLSITES_PHOTOS_ORIGNALS_DIR has not been defined.' . PHP_EOL . PHP_EOL);
+            return;
+
+        }
 
 		echo('Creating database entries...' . PHP_EOL);
 
@@ -164,17 +189,94 @@ class PhotosSync {
 
 	}
 
-	private function generatePhotos($thumbnails = false) {
+    private function deletePhotos($type) {
 
-		if($thumbnails === false) {
+        if($type === self::PHOTO) {
 
-			echo('Generating photos...' . PHP_EOL);
+            $singular = 'photo';
+            $plural = 'photos';
+            $directory = $this->photosDirectory;
 
-		} else {
+        } elseif($type === self::THUMBNAIL) {
 
-			echo('Generating thumbnails...' . PHP_EOL);
+            $singular = 'thumbnail';
+            $plural = 'thumbnails';
+            $directory = $this->thumbnailsDirectory;
 
-		}
+        } else {
+
+            throw new LogicException('Given type must be either a PHOTO or THUMBNAIL.');
+
+        }
+
+        if(defined('CELLSITES_PHOTOS_TARGET_DIR') !== TRUE) {
+
+            echo('Skipping deletion of orphan ' . $plural . ' because constant CELLSITES_PHOTOS_TARGET_DIR has not been defined.' . PHP_EOL);
+            return;
+
+        }
+
+        echo('Deleting orphan ' . $plural . '...' . PHP_EOL);
+
+        $directory->rewind();
+
+        while(FALSE !== ($file = $directory->read())) {
+
+            if(preg_match('/^([1-9][0-9]*)\.jpg$/',$file,$matches) === 1) {
+
+                $target[(int)$matches[1]] = FALSE;
+
+            }
+
+        }
+
+        $photos = PhotoQuery::create()->filterByVisible(true)->find();
+
+        foreach($photos as $thisPhoto) {
+
+            if(isset($target[$thisPhoto->getID()]) === TRUE) {
+
+                unset($target[(int)$thisPhoto->getID()]);
+
+            }
+
+        }
+
+        if(count($target) === 0) {
+
+            echo('There were no ' . $plural . ' to delete.' . PHP_EOL);
+
+        }
+
+        echo(PHP_EOL);
+
+    }
+
+    private function generatePhotos($thumbnails = false) {
+
+        if($thumbnails === false) {
+
+            $plural = 'photos';
+
+        } else {
+
+            $plural = 'thumbnails';
+
+        }
+
+        if(defined('CELLSITES_PHOTOS_ORIGNALS_DIR') !== TRUE) {
+
+            echo('Skipping generation of ' .  $plural . ' because constant CELLSITES_PHOTOS_ORIGNALS_DIR has not been defined.' . PHP_EOL);
+            return;
+
+        } elseif(defined('CELLSITES_PHOTOS_TARGET_DIR') !== TRUE) {
+
+            echo('Skipping generation of ' . $plural . ' because constant CELLSITES_PHOTOS_TARGET_DIR has not been defined.' . PHP_EOL);
+            return;
+
+        }
+
+        echo('Generating ' . $plural . '...' . PHP_EOL);
 
 		$photos = PhotoQuery::create()->filterByVisible(true)->orderByID()->find();
 
@@ -248,15 +350,7 @@ class PhotosSync {
 
 		if($numberOfSkippedPhotos > 0) {
 
-			if($thumbnails === false) {
-
-				echo($numberOfSkippedPhotos . ' photos were skipped because they already existed.' . PHP_EOL);
-
-			} else {
-
-				echo($numberOfSkippedPhotos . ' thumbnails were skipped because they already existed.' . PHP_EOL);
-
-			}
+            echo($numberOfSkippedPhotos . ' ' . $plural . ' were skipped because they already existed.' . PHP_EOL);
 
 		}
 
@@ -264,63 +358,65 @@ class PhotosSync {
 
 	}
 
-	public function process() {
+    public function process() {
 
-		$this->checkOriginalsDirectory();
-		$this->checkPhotosDirectory();
-		$this->checkThumbnailsDirectory();
-		$this->createDatabaseEntries();
-		$this->generatePhotos();
-		$this->generatePhotos(true);
+        $this->checkDirectory(self::ORIGINAL);
+        $this->checkDirectory(self::PHOTO);
+        $this->checkDirectory(self::THUMBNAIL);
+        $this->createDatabaseEntries();
+        $this->deletePhotos(self::PHOTO);
+        $this->deletePhotos(self::THUMBNAIL);
+        $this->generatePhotos();
+        $this->generatePhotos(true);
 
-	}
+    }
 
-	private static function rotateImagick($imagick, $orientation = null) {
+    private static function rotateImagick($imagick, $orientation = null) {
 
-		if($orientation === null) {
+        if($orientation === null) {
 
-			$orientation = $imagick->getImageOrientation();
+            $orientation = $imagick->getImageOrientation();
 
-		}
+        }
 
-		switch($orientation) {
+        switch($orientation) {
 
-			case Imagick::ORIENTATION_TOPRIGHT:
-			$imagick->flopImage();
-			break;
+            case Imagick::ORIENTATION_TOPRIGHT:
+            $imagick->flopImage();
+            break;
 
-			case Imagick::ORIENTATION_BOTTOMRIGHT:
-			$imagick->rotateImage('#000', 180);
-			break;
+            case Imagick::ORIENTATION_BOTTOMRIGHT:
+            $imagick->rotateImage('#000', 180);
+            break;
 
-			case Imagick::ORIENTATION_BOTTOMLEFT:
-			$imagick->flopImage();
-			$imagick->rotateImage('#000', 180);
-			break;
+            case Imagick::ORIENTATION_BOTTOMLEFT:
+            $imagick->flopImage();
+            $imagick->rotateImage('#000', 180);
+            break;
 
-			case Imagick::ORIENTATION_LEFTTOP:
-			$imagick->flopImage();
-			$imagick->rotateImage('#000', -90);
-			break;
+            case Imagick::ORIENTATION_LEFTTOP:
+            $imagick->flopImage();
+            $imagick->rotateImage('#000', -90);
+            break;
 
-			case Imagick::ORIENTATION_RIGHTTOP:
-			$imagick->rotateImage('#000', 90);
-			break;
+            case Imagick::ORIENTATION_RIGHTTOP:
+            $imagick->rotateImage('#000', 90);
+            break;
 
-			case Imagick::ORIENTATION_RIGHTBOTTOM:
-			$imagick->flopImage();
-			$imagick->rotateImage('#000', 90);
-			break;
+            case Imagick::ORIENTATION_RIGHTBOTTOM:
+            $imagick->flopImage();
+            $imagick->rotateImage('#000', 90);
+            break;
 
-			case Imagick::ORIENTATION_LEFTBOTTOM:
-			$imagick->rotateImage('#000', -90);
-			break;
+            case Imagick::ORIENTATION_LEFTBOTTOM:
+            $imagick->rotateImage('#000', -90);
+            break;
 
-		}
+        }
 
-		$imagick->setImageOrientation(Imagick::ORIENTATION_TOPLEFT);
+        $imagick->setImageOrientation(Imagick::ORIENTATION_TOPLEFT);
 
-	}
+    }
 
 }
 
